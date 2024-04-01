d673c52350a0e1341bd6c5a938515a27f4b3e3b3
Jonas Remmert <j.remmert@phytec.de>
2024-04-01 23:56:38 +0200

    app: add console for simulating button press
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

--- app/src/main.c
+++ app/src/main.c
@@ -1,5 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/zbus/zbus.h>
+#include <zephyr/shell/shell.h>
 
 #include "message_channel.h"
 
@@ -41,6 +42,32 @@ static void button_msg_cb(const struct zbus_channel *chan)
 		break;
 	}
 }
+
+static int cmd_button(const struct shell *sh, size_t argc, char **argv,
+		    void *data)
+{
+	int err;
+	enum sys_msg msg;
+
+	msg = SYS_BUTTON_PRESSED;
+
+	err = zbus_chan_pub(&button_ch, &msg, K_SECONDS(1));
+	if (err) {
+		LOG_ERR("zbus_chan_pub, error: %d", err);
+	}
+
+	return 0;
+}
+
+SHELL_SUBCMD_DICT_SET_CREATE(sub_button_cmds, cmd_button,
+	(button_press, 1, "Trigger Button Press")
+);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(my_app_cmds,
+	SHELL_CMD(button, &sub_button_cmds, "Button test commands", NULL),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
+SHELL_CMD_REGISTER(my_app, &my_app_cmds, "my App test", NULL);
 
 int main(void)
 {
