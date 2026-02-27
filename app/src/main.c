9af051ae280165742af5f17263b79396c10dd4b5
Jonas Remmert <j.remmert@mailbox.org>
2026-02-27 21:25:34 +0100

    app: move tracing to a separate module
    Signed-off-by: Jonas Remmert <j.remmert@mailbox.org>

--- app/src/main.c
+++ app/src/main.c
@@ -1,29 +1,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-#include "sample_usbd.h"
-#include <zephyr/device.h>
-#endif
-
 LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
 int main(void)
 {
-#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-	struct usbd_context *sample_usbd = sample_usbd_init_device(NULL);
-
-	if (sample_usbd == NULL) {
-		printk("Failed to initialize USB device\n");
-		return 0;
-	}
-
-	if (usbd_enable(sample_usbd)) {
-		printk("USB backend enable failed\n");
-		return 0;
-	}
-#endif /* CONFIG_USB_DEVICE_STACK_NEXT */
-
 	printk("System booted. Main thread going to sleep.\n");
 	return 0;
 }
