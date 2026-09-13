c39d63f962791d5bf13f3fa260783bf4f06ba594
Stephan Linz <linz@li-pro.net>
2026-09-13 11:05:22 +0200

    feat: this adds the Colorful sample
    This sample uses the Zephyr display API to render an RGB color
    gradient across the entire display.
    
    With most display drivers, the RGB color gradient runs across the
    entire screen, from red through yellow to green, then from green
    through cyan to blue, and finally from blue through magenta back
    to red. This cycle repeats endlessly and changes the color of all
    pixels at once. It is not a static representation of an RGB color
    gradient; the colors simply cycle through.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ samples/hweval/colorful/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Navimatix GmbH
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Based on Zephyr upstream display driver sample:
+ * Copyright (c) 2019 Jan Van Winkel <jan.van_winkel@dxplore.eu>
+ * Copyright (c) 2025 NXP
+ */
+
+#include "colorful.h"
+
+int main(void)
+{
+	return sample_colorful_draw();
+}
